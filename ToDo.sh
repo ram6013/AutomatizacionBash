be5572a3dc546d0017aee99747c706cdbe469f69
ram6013 <ramongarciaprado2002@gmail.com>
2024-11-28 12:40:58 +0100

    Desarrollo de medio interfaz y adición de alguna funcion más

--- ToDo.sh
+++ ToDo.sh
@@ -1,112 +1,152 @@
-taskFile="$HOME/Documentos/Task/tasks.txt"
-checkFile="$HOME/Documentos/Task/done.txt "
+basePath="$HOME/Documentos/Task"
 
-
-add(){
-    if [ -z "$1" ]; then
-        echo "Argumentos recibidos: $1 , $2"
-        echo "Error: Necesitas proporcionar un nombre para la tarea."
-        echo "Uso: $0 add <nombre>"
+comprobacionAdd(){
+    if [ -z "$2" ]; then
+        echo "Error: Necesitas proporcionar un nombre para el nuevo espacio."
+        echo "Uso: toDo add <nombre>"
         return 1
     fi
-    if [ ! -f "$taskFile" ]; then
-        touch "$taskFile"
+    return 0
+}
+
+comprobacion(){
+    if [ ! -z "$2" ]; then
+        echo "No hace falta añadir nada, se continura con el proceso por default."
     fi
+}
+add(){
+    if [ -z "$1" ]; then
+        echo "Error: Necesitas proporcionar un nombre para la tarea."
+        echo "Uso: toDo add <nombre>"
+        return 1
+    fi
+    selected=$(ls "$basePath" | fzf)
     task="$@"
-    if grep -q "$task" "$taskFile"; then
+    if [ -z "$selected" ]; then
+        echo "Error no se ha seleccionado nada"
+        return 1
+    fi
+    if grep -q "$task" "$basePath/$selected"; then
         echo "Error"
         echo "La tarea '$@' ya existe en la lista."
         return 1
     fi
 
-    echo "$@" >> "$taskFile"
-    echo "Se ha añadido '$@' a la lista de tareas."
+    echo "$@" >> "$basePath/$selected"
+    echo "Se ha añadido '$@' a la lista $selected."
 }
 list(){
-
-    if [ ! -s "$taskFile" ]; then
-        echo "No hay tareas en la lista."
-    else
-        echo "=====Tareas pendientes:======"
-        cat "$taskFile"
-        echo "===========Done:============="
-        cat "$checkFile"
-    fi
+    selected=$(ls "$basePath" | fzf)
+    cat "$basePath/$selected"
+    
 }
 remove(){
-    if [ -z "$1" ]; then
-        echo "Error: Necesitas proporcionar un nombre para la tarea que quieres borrar."
-        echo "Uso: $0 remove <nombre>"
+    selected=$(ls "$basePath" | fzf)
+    task=$(cat "$basePath/$selected" | fzf)
+    if [ -z "$task" ]; then
+        echo "No se ha seleccionado nada"
         return 1
     fi
-    task="$@"
-    if ! grep -qxF "$task" "$taskFile"; then
-        echo "Error: No se encontró la tarea '$@'."
-        return 1
-    fi
-    sed -i "/$task/d" "$taskFile"
+    sed -i "/$task/d" "$basePath/$selected"
     echo "Tarea '$task' borrada."
 }
 check(){
-    if [ -z "$1" ]; then 
-        echo "Error: Necesitas proporcionar el nombre que la tarea que has realizado."
-        echo "Uso: $0 done <nombre>"
+    selected=$(find "$basePath" -type f -name "*_task*" | fzf)
+    eleccion=$(cat "$selected" | fzf)
+    if [ -z "$eleccion" ]; then
+        echo "No se ha seleccionado nada"
         return 1
     fi
-    task="$@"
-    if ! grep -qxF "$task" "$taskFile"; then
-        echo "No se ha encontrado la tarea"
-        return 1
-    fi 
-    echo "$task" >> "$checkFile"
-    sed -i "/$task/d" "$taskFile"
-    echo "Check '$task' realizada."
+    direccion=$(find "$basePath" -type f -name "*_done*" | fzf)
+    echo "$eleccion" >> "$direccion"
+    sed -i "/$eleccion/d" "$selected"
+    echo "Check '$eleccion' realizada."
 }
 
 clear(){
-    if [ -z "$1" ]; then
-            echo "Error: Necesitas proporcionar cual quieres vaciar ToDo o Done."
-            echo "Uso: toDo clear <ToDo|Done>"
-            return 1
-        fi
-        if [ "$1" == "ToDo" ]; then
-            > "$taskFile"
-            echo "Se han borrado todas las ToDo."
-        elif [ "$1" == "Done" ]; then
-            > "$checkFile"
-            echo "Se han borrado todas las Done."
-        else
-            echo "Error: Argumento no reconocido."
-            echo "Uso: toDo clear <ToDo|Done>"
-        fi
+    selected=$(ls "$basePath" | fzf)
+    if [ -z "$selected" ]; then
+        echo "No se ha seleccionado nada"
+        return 1
+    fi
+    > "$basePath/$selected"
+    echo "Se ha vaciado $selected."
+}
+
+create(){
+    files=$(ls $basePath)
+    file1="${1}_task.txt"
+    file2="${1}_done.txt"
+    if echo "$files" | grep -q -w "$file1" || echo "$files" | grep -q -w "$file2"; then
+        echo "Error"
+        echo "El espacio ya existe en la lista."
+        return 1
+    fi
+    echo "===========LISTA DE TAREAS===========" >> "$basePath/$file1"
+    echo "===========LISTA DE DONE=============" >> "$basePath/$file2"
+    echo "Se ha creado el espacio $file1 y el espacio $file2."
+}
+destroy(){
+    selected=$(ls "$basePath" | fzf)
+    if [ -z "$selected" ]; then
+        echo "No se ha seleccionado nada"
+        return 1
+    fi
+    rm "$basePath/$selected"
+    if [[ "$selected" == *_done.txt ]]; then
+        rm -f "$basePath/${selected/_done.txt/_task.txt}"
+    elif [[ "$selected" == *_task.txt ]]; then
+        rm -f "$basePath/${selected/_task.txt/_done.txt}"
+    fi
+    echo "Se ha borrado con éxito."
 }
 case $1 in
     add)
-        add $2
+        comprobacionAdd $@
+        if [ $? -eq 0 ]; then  # Verifica si la función comprobacionAdd devolvió 0
+            add "$2"
+        fi
         ;;
     list)
+        comprobacion $@
         list
         ;;
     remove)
-        remove $2
+        comprobacion $@
+        remove
         ;;
     done)
-        check $2
+        comprobacion $@
+        check
         ;;
     clear)
-        clear $2
+        comprobacion $@
+        clear
+        ;;
+    create)
+
+        comprobacionAdd $@
+        if [ $? -eq 0 ]; then  # Verifica si la función comprobacionAdd devolvió 0
+            add "$2"
+        fi
+        ;;
+    destroy)
+        comprobacion $@
+        destroy
         ;;
     help)
         echo -e "   Uso:      toDo [add|list|remove|done|clear]\n"
-        echo -e "   add:      Añadir una tarea a la lista. Se debe especificar el nombre de la tarea. Ejemplo: toDo add 'Comprar pan'"
-        echo -e "   list:     Mostrar la lista de tareas y de Done.\n"
-        echo -e "   remove:   Borrar una tarea de la lista. Se debe especificar el nombre de la tarea. Ejemplo: toDo remove 'Comprar pan'\n"
-        echo -e "   done:     Marcar una tarea como realizada. Se debe especificar el nombre de la tarea. Ejemplo: toDo done 'Comprar pan'\n"
-        echo -e "   clear:    Limpiar la lista de tareas o de Done. Se debe especificar cual quieres limpiar ToDo o Done. Ejemplo: toDo clear ToDo\n"
+        echo -e "   add:      Añadir una tarea a la lista. Se debe especificar el nombre de la tarea y luego seleccionar donde añadir. Ejemplo: toDo add 'Comprar pan'"
+        echo -e "   list:     Mostrar las listas de tareas y de Done.Se debe de seleccionar cual ver.\n"
+        echo -e "   remove:   Borrar una tarea de la lista. Se debe elegir el nombre de la tarea que se quiere borrar y de donde. Ejemplo: toDo remove\n"
+        echo -e "   done:     Marcar una tarea como realizada. Se debe elegir el nombre de la tarea. Ejemplo: toDo done\n"
+        echo -e "   clear:    Limpiar la lista de tareas o de Done. Se debe elegir cual quieres limpiar. Ejemplo: toDo clear\n"
+        echo -e "   create:   Crear un nuevo espacio. Se debe especificar el nombre del espacio. Ejemplo: toDo create 'Universidad\n'."
+        echo -e "   destroy:  Eliminar un espacio. Se debe elegir el espacio a eliminar. Ejemplo: toDo destroy\n"
         echo -e "   help:     Mostrar ayuda."
         ;;
     *)
         echo "Comando no reconocido"
-        echo "Uso: toDo [add|list|remove] <argumentos>"
+        echo "Uso: toDo [add|remove|done|list|clear|create|help] <argumentos>"
         ;;
 esac
--- toDo_tabs.sh
+++ toDo_tabs.sh
@@ -1,7 +1,7 @@
 _toDo_completions() {
     local cur="${COMP_WORDS[COMP_CWORD]}"
 
-    local commands="add done list remove clear help"
+    local commands="create add done list remove clear destroy help"
 
     COMPREPLY=( $(compgen -W "$commands" -- "$cur") )
 }
