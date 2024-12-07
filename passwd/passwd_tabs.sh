32133d13fa9e38aea718b55dddf02f7d5b0310c0
ram6013 <134057913+ram6013@users.noreply.github.com>
2024-12-07 14:04:46 +0100

    Delete passwd directory

--- passwd/passwd_tabs.sh
+++
@@ -1,9 +0,0 @@
-_passwd_completions() {
-    local cur="${COMP_WORDS[COMP_CWORD]}"
-
-    local commands="generar visualizar eliminar"
-
-    COMPREPLY=( $(compgen -W "$commands" -- "$cur") )
-}
-
-complete -F _passwd_completions passwd
